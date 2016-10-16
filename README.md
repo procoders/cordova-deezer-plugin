08ff217f9475c3a51e6d510879c541880819d4d2
aleksey stepanets <aleksey stepanets>
2016-10-16 21:15:11 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Hooks
+
+For use it plugin:
+1. Copy the contents from index.js file to your index.js file
+2. 
---
+++ misc/index.js
@@ -0,0 +1,34 @@
+var deezerPlayer={
+         myInit:function(callbackSaccess,callbackError, appId){
+         cordova.exec(callbackSaccess, callbackError, "Deezer", "init", [appId])
+         },
+        myPlayAlbum : function(callbackSaccess,callbackError,albumId){
+                cordova.exec(callbackSaccess, callbackError, "Deezer", "playerControl", [{"offset" : 0, "index" : 0, "autoplay" : true , "addToQueue" : false, "album_id" : albumId}, "playAlbum"])
+         },
+        myPlayPlaylist : function(callbackSaccess, callbackError,playListId){
+            cordova.exec(callbackSaccess, callbackError,"Deezer", "playerControl",[{"offset" : 0, "index" : 0, "autoplay" : true, "addToQueue" : false, "playlist_id" : playListId}, "playPlaylist"]);
+        },
+        myPlayRadio : function(callbackSaccess, callbackError,radioId){
+            cordova.exec(callbackSaccess, callbackError,"Deezer", "playerControl",
+            [{"offset" : 0, "index" : 0, "autoplay" : true, "addToQueue" : false, "radio_id" : "28"}, "playRadio"]);
+        },
+        myPause : function(callbackSaccess, callbackError){
+             cordova.exec(callbackSaccess, callbackError, "Deezer", "doAction", [{"command" : "pause"}]);
+        },
+        myPlay : function(callbackSaccess, callbackError){
+            cordova.exec(callbackSaccess, callbackError, "Deezer", "doAction", [{"command" : "play"}]);
+        },
+        myPrev : function(callbackSaccess, callbackError){
+            cordova.exec(callbackSaccess, callbackError, "Deezer", "doAction", [{"command" : "prev"}]);
+        },
+        myNext : function(callbackSaccess, callbackError){
+            cordova.exec(callbackSaccess, callbackError, "Deezer", "doAction", [{"command" : "next"}]);
+        },
+        myPlayTracks :function(callbackSaccess, callbackError,tracksId){
+            cordova.exec(callbackSaccess, callbackError, "Deezer","playerControl", [{"offset" : 0, "index" : 0, "autoplay" : true , "addToQueue" : false, "trackList" : tracksId}, "playTracks"])
+        },
+        onPosition : function(player_position){
+            cordova.exec(null,null,"Deezer","playerControl",[{"offset" : player_position,"index" : 0, "autoplay" : true , "addToQueue" : false, "chengPosition" : player_position}, "chengPosition"])
+        },
+
+        };
